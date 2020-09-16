35a36eccce28e353eb1dd0af24232328d196e3ab
Natalie Chen <nataliekc71@gmail.com>
2020-09-16 12:07:26 -0700

    Add search function to pins model

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -8,9 +8,25 @@ class Pin < ApplicationRecord
   has_many :boards, through: :pins_on_boards, source: :board
   belongs_to :user
 
+  #sanitize my input
+  def self.search(param)
+    
+    str = "%#{param}%".downcase
+    #debugger
+    @pins = Pin.where(
+            "LOWER(TITLE) LIKE LOWER(:query) OR 
+            LOWER(DESCRIPTION) LIKE LOWER(:query)", query:str)
+    
+  end
+
   def has_image
     unless self.image.attached?
       errors[:image] << 'Attach an Image ^.^'
     end
   end
 end
+
+#"LOWER(TITLE) LIKE LOWER(?) OR 
+#LOWER(DESCRIPTION) LIKE LOWER(?)", str, str
+#like_keyword = "%#{params[:keyword]}%".downcase
+#@pins = Pin.where("LOWER(TITLE) LIKE ? OR LOWER(DESCRIPTION) LIKE ?", like_keyword, like_keyword)
