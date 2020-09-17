ab8e25ca7e2874df27be9df227c1157b2c063d6c
Natalie Chen <nataliekc71@gmail.com>
2020-09-17 13:17:59 -0700

    Add search functionality inside the navigation bar

--- frontend/components/navigation_bar/navbar.jsx
+++ frontend/components/navigation_bar/navbar.jsx
@@ -116,7 +116,132 @@ class NavBar extends React.Component {
                     </div>
                 </div>
             )
+    this.state = {searchWord: ""}
+    this.onKeyUp = this.onKeyUp.bind(this);
+  onKeyUp(e) {
+    //debugger;
+    e.preventDefault();
+    if (e.key !== "Enter") {
+      this.setState({ searchWord: e.currentTarget.value });
+      this.props.fetchSearchPins(this.state.searchWord);
+      //this.props.history.push("/search");
+      //below redierects you so your cursor will be out of the input field
+      this.props.history.push({
+        pathname: "/search",
+        state: { searchWord: e.currentTarget.value },
+      });
+    } else {
+      $(".search-input").val("");
+      this.setState({ searchWord: "" });
     }
+  render() {
+    const { currentUser, logout } = this.props;
+    // const { logout } = this.props;
+    // const currentUser = this.state;
+
+    const profilePhoto =
+      currentUser && currentUser.photo ? (
+        <img src={currentUser.photo} alt="profile-pic" id="profile-icon-main" />
+      ) : (
+        //<i className="fas fa-user-circle" id="profile-icon" style={{ "color": "#8e8e8e" }}></i>
+        <div className="profile-icon">
+          {currentUser.username[0].toUpperCase()}
+        </div>
+      );
+
+    return !currentUser ? null : (
+      <div className="nav-bar-background">
+        <div className="nav-bar-wrapper">
+          <header className="nav-bar-container">
+            <div className="nav-bar-button" id="home">
+              <Link to="/feed" className="nav-bar-link">
+                <div className="icon-container-shadow">
+                  <div className="icon-container">
+                    <img src={window.logo} id="home-icon"></img>
+                  </div>
+                </div>
+              </Link>
+            </div>
+
+            <div className="nav-bar-search-container" id="search">
+              <div className="search-container">
+                <div className="search-icon-container">
+                  <i className="fas fa-search" id="search-icon"></i>
+                </div>
+                <div className="search-input-container">
+                  <input
+                    className="search-input"
+                    type="text"
+                    value={this.state.searchWord}
+                    placeholder="Search"
+                  />
+                  {/* <Search/> redner the search results inside navbar */}
+                </div>
+              </div>
+            </div>
+
+            <div className="right-nav-bar">
+              <div className="nav-bar-button" id="profile">
+                <NavLink
+                  to={`/${currentUser.username}`}
+                  className="nav-bar-link"
+                >
+                  <div className="icon-container-shadow">
+                    <div className="icon-container">
+                      <div id="profile-icon-frame">{profilePhoto}</div>
+                    </div>
+                  </div>
+                </NavLink>
+              </div>
+
+              <div className="nav-bar-spacer" id="spacer"></div>
+
+              <div className="nav-bar-button" id="github">
+                <a
+                  href="https://github.com/NatalieCC"
+                  target="_blank"
+                  className="nav-bar-link"
+                >
+                  <div className="icon-container-shadow">
+                    <div className="icon-container">
+                      <i
+                        className="fab fa-github-square"
+                        id="networking-icon"
+                      ></i>
+                    </div>
+                  </div>
+                </a>
+              </div>
+
+              <div className="nav-bar-button" id="linkedin">
+                <a
+                  href="https://www.linkedin.com/in/nataliecake/"
+                  target="_blank"
+                  className="nav-bar-link"
+                >
+                  <div className="icon-container-shadow">
+                    <div className="icon-container">
+                      <i className="fab fa-linkedin" id="networking-icon"></i>
+                    </div>
+                  </div>
+                </a>
+              </div>
+
+              <div className="nav-bar-button" id="options">
+                <Link to="/" className="nav-bar-link" onClick={logout}>
+                  <div className="icon-container-shadow">
+                    <div className="icon-container">
+                      <i className="fas fa-sign-out-alt" id="options-icon"></i>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </header>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default NavBar;
